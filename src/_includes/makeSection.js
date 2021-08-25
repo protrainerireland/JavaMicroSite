888e1f3e517696f51f4c675af64cdd357f95d7ad
Calum <calumjamesoneill@gmail.com>
2021-08-25 10:43:34 +0100

    Added link to the default template

--- src/_includes/makeSection.js
+++ src/_includes/makeSection.js
@@ -41,7 +41,7 @@ module.exports = {
             
             case "keywordtext":
 
-                let defaultTemplate = `${site.searchKeywords[0]} is one of the most popular coding languages in the world. The job market for ${ site.searchKeywords[0] } developers is robust and consistent, making it a great programming language to learn. Our courses can give you the skills you need to work effectively with ${ site.searchKeywords[0] }.  Why attend one of our ${ site.searchKeywords[0] } courses? If you're interested working effectively with ${ site.searchKeywords[0] } to benefit your organisation and to get an edge over your competitors, or simply to learn a highly lucrative skill then you should consider our ${ site.searchKeywords[0] } training course.</p>`
+                let defaultTemplate = `${site.searchKeywords[0]} is one of the most popular coding languages in the world. The job market for ${ site.searchKeywords[0] } developers is robust and consistent, making it a great programming language to learn. Our courses can give you the skills you need to work effectively with ${ site.searchKeywords[0] }.  Why attend one of our ${ site.searchKeywords[0] } courses? If you're interested working effectively with ${ site.searchKeywords[0] } to benefit your organisation and to get an edge over your competitors, or simply to learn a highly lucrative skill then you should consider our <a href = /courses >${ site.searchKeywords[0] } training courses.</a></p>`
                 
                 html = `<section id="${section.id}" class="section">
                     <div class="container">
